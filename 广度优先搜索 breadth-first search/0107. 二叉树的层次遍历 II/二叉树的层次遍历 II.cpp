2e611b4ada8377532f1487e4b7fe73ae1c52907b
caitouwww <wangwetro@gmail.com>
2020-03-23 11:22:30 +0800

    add cpp file

---
+++ 广度优先搜索 breadth-first search/0107. 二叉树的层次遍历 II/二叉树的层次遍历 II.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(root==NULL)
+            return {};
+        vector<vector<int>> res;
+        vector<TreeNode*> stack(1,root);
+        while(stack.size()!=0)
+        {
+            vector<TreeNode*> tmp;
+            vector<int> res_each;
+            for(auto x:stack)
+            {
+                res_each.push_back(x->val);
+                if(x->left!=NULL)
+                    tmp.push_back(x->left);
+                if(x->right!=NULL)
+                    tmp.push_back(x->right);
+            }
+            stack=tmp;
+            res.insert(res.begin(),res_each);
+        }
+        return res;
+    }
+};

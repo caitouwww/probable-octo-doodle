39e4423534562bab399a9159c9a9fb76cd4dabba
caitouwww <wangwetro@gmail.com>
2020-03-28 10:02:43 +0800

    add cpp file

--- 树 tree/0257. 二叉树的所有路径/二叉树的所有路径.cpp
+++ 树 tree/0257. 二叉树的所有路径/二叉树的所有路径.cpp
@@ -12,7 +12,7 @@ public:
     //vector<string> vec;
     //string path;
     vector<string> binaryTreePaths(TreeNode* root) {
-        //µÝ¹é
+        //??
         /*if(!root)
             return vec;
         if(!root->left&&!root->right)
@@ -28,8 +28,8 @@ public:
         binaryTreePaths(root->right);
         path=path2;
         return vec;*/
-        //µü´ú
-        vector<string> ans;
+        //???,???
+        /*vector<string> ans;
         if(root==NULL) return ans;
         TreeNode* p=root;
         stack<pair<TreeNode*,string>> s;
@@ -49,6 +49,25 @@ public:
             if(p->right==NULL&&p->left==NULL) ans.push_back(str);
             p=p->right;
         }
-        return ans;
+        return ans;*/
+        //??
+        vector<string> res;
+        if(root==NULL)
+            return res;
+        queue<pair<TreeNode*,string>> q;
+        q.push(make_pair(root,to_string(root->val)));
+        while(!q.empty())
+        {
+            TreeNode* node=q.front().first;
+            string path=q.front().second;
+            q.pop();
+            if(node->left==NULL&&node->right==NULL)
+                res.push_back(path);
+            if(node->left)
+                q.push(make_pair(node->left,path+"->"+to_string(node->left->val)));
+            if(node->right)
+                q.push(make_pair(node->right,path+"->"+to_string(node->right->val)));
+        }
+        return res;
     }
 };

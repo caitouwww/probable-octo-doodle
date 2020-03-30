1573e98e6bbc8fc3e00432cda66852d451f9f301
caitouwww <wangwetro@gmail.com>
2020-03-30 20:15:11 +0800

    add cpp file

---
+++ 贪心算法 greedy/1217. 玩筹码/玩筹码.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& chips) {
+        int odd=0,even=0;
+        for(int i=0;i<chips.size();i++)
+        {
+            if(chips[i]%2==1)
+                odd++;
+            else
+                even++;
+        }
+        return min(odd,even);
+    }
+};
